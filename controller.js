cfa0b2f886eaff0ab68c13a570aca8692458b070
Facundo Abarca <Facundo.Abarca@tipsalud.local>
2016-09-27 15:59:06 -0300

    index.html

--- controller.js
+++ controller.js
@@ -1,4 +1,30 @@
-angular.module('test', [])
-.controller('testController', ['$scope', function($scope) {
-	
+var app = angular.module('test', [])
+
+app.controller('testController', ['$scope', function($scope) {
+
+	$scope.arrayNumbers = [];
+	$scope.minNumber = null;
+	$scope.maxNumber = null;
+	$scope.classFilter = "";
+	$scope.classListDisplay = 'left';
+
+	$scope.filterFn = function(number)
+	{
+  		switch($scope.classFilter){
+  			case 'odd':
+  			 if (number % 2 !== 0) {
+  			  	return true;	
+  			  }
+  				break;
+  			case 'even':
+  			  if (number%2 == 0) {
+  			  	return true;	
+  			  }
+  				break;
+  			default: 
+  				return true 
+  		}
+	};	
 }]);
+
+
